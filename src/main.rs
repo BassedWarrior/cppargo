c572eba4ba4d4c5a4ed5304b3fdcdfc9d24861aa
BassedWarrior <bassedwarrior724@outlook.com>
2024-12-18 16:56:45 -0600

    feat: improve error context using `anyhow`.
    The new implementations of the `anyhow` package use it to better extent
    in order for the context to be referencing the errors thrown by the
    different parts of the project. Giving the user a better insight into
    the nature of the error.

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,19 @@ use anyhow::{self, Context};
 /// - A `target` directory in which the compiled binaries are stored and run.
 fn new_project(name: &str) -> anyhow::Result<()> {
     let project_dir: &Path = Path::new(name);
-    if project_dir.exists() {
-        return Ok(());
-    }
 
-    fs::create_dir(project_dir)
-        .with_context(|| format!("Failed to create project directory."))?;
+    fs::create_dir(project_dir).with_context(|| {
+        format!(
+            "Failed to create project directory {}.",
+            project_dir.display()
+        )
+    })?;
 
     fs::create_dir(project_dir.join("src"))
-        .with_context(|| format!("Failed to create project source directory."))?;
+        .with_context(|| "Failed to create project source directory.")?;
 
     fs::create_dir(project_dir.join("target"))
-        .with_context(|| format!("Failed to create project target directory."))?;
+        .with_context(|| "Failed to create project target directory.")?;
 
     let hello_world_program = concat!(
         "#include <iostream>\n",
@@ -37,7 +38,7 @@ fn new_project(name: &str) -> anyhow::Result<()> {
         project_dir.join("src").join("main.cpp"),
         hello_world_program,
     )
-    .with_context(|| format!("Failed to create project `main.cpp` file."))?;
+    .with_context(|| "Failed to create project `main.cpp` file.")?;
 
     Ok(())
 }
@@ -48,49 +49,50 @@ fn new_project(name: &str) -> anyhow::Result<()> {
 /// and gives it to the `g++` compiler to store it in the `target` directory
 /// with the project name.
 fn build_project() -> anyhow::Result<()> {
-    let project_dir =
-        env::current_dir().with_context(|| format!("Couldn't get project directory."))?;
+    let project_dir = env::current_dir().with_context(|| "Couldn't get project directory.")?;
     let project_src = project_dir.join("src");
     let project_target = project_dir.join("target");
 
-    if !(project_src.exists() && project_target.exists()) {
-        anyhow::bail!("No src and target directories.");
-    };
+    anyhow::ensure!(
+        project_src.exists() && project_target.exists(),
+        format!(
+            "No src {} and target {} directories.",
+            project_src.display(),
+            project_target.display()
+        )
+    );
 
-    let src_files = match fs::read_dir(project_src) {
-        Ok(files) => files
-            .filter_map(|f| f.ok())
-            .map(|f| f.path())
-            .filter(|f| f.extension().unwrap() == "cpp"),
-        Err(_) => anyhow::bail!("Couldn't read source directory."),
-    };
+    let src_files = fs::read_dir(&project_src)
+        .with_context(|| format!("Couldn't read source directory {}.", &project_src.display()))?
+        .filter_map(|f| f.ok())
+        .map(|f| f.path())
+        .filter(|f| f.extension().unwrap() == "cpp");
 
     let project_name = match project_dir.file_name() {
         Some(name) => name,
-        None => anyhow::bail!("Couldn't get project name."),
+        None => anyhow::bail!(format!(
+            "Couldn't get project name from {}.",
+            project_dir.display()
+        )),
     };
 
     let output_file = project_target.join(project_name);
 
-    let output_args = ["-o", output_file.to_str().unwrap()];
+    let output_args = [
+        "-o",
+        output_file
+            .to_str()
+            .expect("Should be able to convert output_file to string"),
+    ];
 
-    match Command::new("g++")
-        .args(output_args)
-        .args(src_files)
-        .spawn()
-    {
-        Ok(mut compiler) => {
-            match compiler.wait() {
-                Ok(exit_status) => {
-                    if !exit_status.success() {
-                        anyhow::bail!("Compilation failed");
-                    }
-                }
-                Err(_) => anyhow::bail!("Compiler couldn't run properly."),
-            };
-        }
-        Err(_) => anyhow::bail!("Couldn't start compiler."),
-    };
+    let mut compiler = Command::new("g++");
+    compiler.args(output_args).args(src_files);
+    println!("Running compiler...\n{:?}", &compiler);
+    let compiler_status = compiler
+        .status()
+        .with_context(|| format!("Couldn't start compiler: {:?}", compiler))?;
+
+    anyhow::ensure!(compiler_status.success(), "Compilation failed!");
 
     Ok(())
 }
@@ -100,10 +102,7 @@ fn build_project() -> anyhow::Result<()> {
 /// Call the `build_project()` function to compile the project, and then
 /// excecutes the compiled binary stored in the `target` directory.
 fn run_project() -> anyhow::Result<()> {
-    let project_dir = match env::current_dir() {
-        Ok(path) => path,
-        Err(_) => anyhow::bail!("Couldn't access project directory."),
-    };
+    let project_dir = env::current_dir().with_context(|| "Couldn't access project directory.")?;
 
     let project_target = project_dir.join("target");
 
@@ -114,10 +113,21 @@ fn run_project() -> anyhow::Result<()> {
 
     let project_excecutable = project_target.join(project_name);
 
-    let _ = match Command::new(project_excecutable).spawn() {
-        Ok(mut child) => child.wait(),
-        Err(_) => anyhow::bail!("Couldn't excecute project file."),
-    };
+    Command::new(&project_excecutable)
+        .spawn()
+        .with_context(|| {
+            format!(
+                "Couldn't excecute project file {}",
+                &project_excecutable.display()
+            )
+        })?
+        .wait()
+        .with_context(|| {
+            format!(
+                "Project file {} wasn't running",
+                &project_excecutable.display()
+            )
+        })?;
 
     Ok(())
 }
@@ -126,34 +136,24 @@ fn main() -> anyhow::Result<()> {
     let mut args = env::args();
     args.next(); // Discard excecutable name
 
-    let command = args.next().expect("No command provided.");
+    let command = args.next().with_context(|| "No command provided.")?;
 
     if command == "new" {
         let project_name = args.next().expect("No project name provided.");
         println!("Creating new project {project_name}...");
-        match new_project(&project_name) {
-            Ok(_) => println!("Project {project_name} created succesfully!"),
-            Err(err) => anyhow::bail!("{err}")
-        };
+        new_project(&project_name)
+            .with_context(|| format!("Failed to create project {}", &project_name))?;
+        println!("Project {project_name} created succesfully!");
     } else if command == "build" {
         println!("Building project...");
-        match build_project() {
-            Ok(_) => println!("Project built succesfully!"),
-            Err(err) => anyhow::bail!("{err}"),
-        };
+        build_project().with_context(|| "Failed to build project.")?;
+        println!("Project built succesfully!");
     } else if command == "run" {
         println!("Building project...");
-        match build_project() {
-            Ok(_) => println!("Project built succesfully!"),
-            Err(err) => {
-                eprintln!("{err}");
-                anyhow::bail!("Cannot run project");
-            }
-        };
+        build_project().with_context(|| "Failed to build project before attempting to run it.")?;
+        println!("Project built succesfully!");
         println!("Running project...");
-        if let Err(err) = run_project() {
-            anyhow::bail!("{err}");
-        };
+        run_project().with_context(|| "Failed to run project")?;
     } else {
         anyhow::bail!("Invalid command provided.");
     };

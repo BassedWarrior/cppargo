9af0680022828a97c8210eb51eb9e1bb03bebb8a
BassedWarrior <bassedwarrior724@outlook.com>
2024-12-26 19:05:19 -0600

    feat: add aliases to subcommands.

--- src/main.rs
+++ src/main.rs
@@ -19,17 +19,20 @@ enum Commands {
     /// This command will create a new cppargo project in the given directory.
     /// This includes a sample source file "Hello World!" program
     /// (`src/main.cpp`).
+    #[command(visible_alias = "n")]
     New { path: String },
     /// Compile a local project.
     ///
     /// Iterates over the `src` directory in order to find all `.cpp` source files
     /// and gives it to the `g++` compiler to store it in the `target` directory
     /// with the project name.
+    #[command(visible_alias = "b")]
     Build,
     /// Run a binary of the local project.
     ///
     /// Compile the project, and then excecute the compiled binary stored in
     /// the `target` directory.
+    #[command(visible_alias = "r")]
     Run,
 }
 

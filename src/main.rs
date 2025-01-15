6a1c1344167cf3f582580d572b0626635e080d6c
BassedWarrior <116653989+BassedWarrior@users.noreply.github.com>
2025-01-15 12:20:51 -0600

    fix: fix spelling mistakes

--- src/main.rs
+++ src/main.rs
@@ -16,19 +16,19 @@ fn main() -> anyhow::Result<()> {
             println!("Creating new project {path}...");
             new::new_project(&env::current_dir().unwrap(), &path)
                 .with_context(|| format!("Failed to create project {}", &path))?;
-            println!("Project {path} created succesfully!");
+            println!("Project {path} created successfully!");
         }
         Commands::Build => {
             println!("Building project...");
             build::build_project(&env::current_dir()?)
                 .with_context(|| "Failed to build project.")?;
-            println!("Project built succesfully!");
+            println!("Project built successfully!");
         }
         Commands::Run => {
             println!("Building project...");
             build::build_project(&env::current_dir()?)
                 .with_context(|| "Failed to build project before attempting to run it.")?;
-            println!("Project built succesfully!");
+            println!("Project built successfully!");
             println!("Running project...");
             run::run_project(&env::current_dir()?).with_context(|| "Failed to run project")?;
         }
